f5b2f87ca52a75c533ad7f012f5e961851776135
SuKyoung Shin <sukyoung.dev@gmail.com>
2025-10-03 21:37:20 +0900

    feat: auth.js 로그인 로직 추가1

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -1,8 +1,55 @@
 import { prisma } from "@/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { comparePassword } from "./lib/password-utils";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  useSecureCookies: process.env.NODE_ENV === "production",
+  trustHost: true,
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  secret: process.env.AUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "이메일", type: "email", placeholder: "이메일 입력" },
+        password: {
+          label: "비밀번호",
+          type: "password",
+          placeholder: "비밀번호 입력",
+        },
+      },
+
+      async authorize(credentials) {
+        // 1. 이메일과 비밀번호가 입력되었는지 확인
+        if (!credentials || !credentials.email || !credentials.password) {
+          throw new Error("이메일과 비밀번호를 입력해주세요.");
+        }
+
+        // 2. DB에서 유저를 찾기
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        // 3. 유저가 존재하지 않으면 에러 발생
+        if (!user) {
+          throw new Error("이메일 또는 비밀번호가 일치하지 않습니다.");
+        }
+
+        // 4. 비밀번호가 일치하는지 확인
+        const passwordMatch = comparePassword(credentials.password as string, user.hashedPassword as string);
+        if (!passwordMatch) {
+          throw new Error("비밀번호가 일치하지 않습니다.");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {},
+  callbacks: {}
 });
---
+++ frontend/lib/password-utils.ts
@@ -0,0 +1,15 @@
+import bcrypt from "bcryptjs";
+
+// 비밀번호 해싱
+export function saltAndHashPassword(password: string): string {
+  const saltRounds = 10;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hashedPassword = bcrypt.hashSync(password, salt);
+
+  return hashedPassword;
+}
+
+// DB에 있는 비밀번호와 입력한 비밀번호가 일치하는지 확인
+export function comparePassword(password: string, hashedPassword: string): boolean {
+  return bcrypt.compareSync(password, hashedPassword);
+}
