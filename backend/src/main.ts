a0832ca4ee6ae0e6fd52e71d78d69c62b5d05f69
SuKyoung Shin <sukyoung.dev@gmail.com>
2025-10-05 22:27:29 +0900

    feat: backend swagger 추가

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Inflearn API 문서')
+    .setDescription('Inflearn API 문서입니다.')
+    .setVersion('1.0')
+    .addTag('Inflearn')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'access-token',
+        description: 'access-token을 사용하세요.',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
