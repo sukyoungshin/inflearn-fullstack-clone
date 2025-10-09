627dc6c6cf222327cc743cd16c9886d90421fd6e
SuKyoung Shin <sukyoung.dev@gmail.com>
2025-10-09 12:51:19 +0900

    feat: frontend API테스트 페이지 삭제

--- frontend/hooks/useApi.ts
+++ frontend/hooks/useApi.ts
@@ -1,15 +1,2 @@
 'use client';
 
-import * as api from '@/lib/api';
-import { getCookie } from "cookies-next/client";
-
-const AUTH_COOKIE_NAME = process.env.NODE_ENV === 'production' ? "__Secure-authjs.session-token": 'authjs.session-token';
-
-export function useApi() {
-  // 클라이언트에서 쿠키 가져오기
-  const token = getCookie(AUTH_COOKIE_NAME);
-
-  return {
-    getUserTest: () => api.getUserTest(token),
-  };
-}
--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,12 +1,2 @@
 'use server';
 
-import { appControllerGetUserTest } from '@/generated/openapi-client';
-
-export async function getUserTest(token?: string) {
-  const {data, error} = await appControllerGetUserTest();
-
-  return {
-    data,
-    error,
-  }
-}
