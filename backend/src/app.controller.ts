1eb1e3194e5fe8302d09b5eea088d83ba9284e99
SuKyoung Shin <sukyoung.dev@gmail.com>
2025-10-09 15:03:42 +0900

    feat: backend 스웨거에 유저테스트용 API 추가

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,6 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { AccessTokenGuard } from './auth/guards/access-token.guard';
 
 @ApiTags('App')
 @Controller()
@@ -10,5 +16,17 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('user-test')
+  @UseGuards(AccessTokenGuard)
+  @ApiBearerAuth('access-token')
+  @ApiOperation({ summary: '로그인 유저 이메일' })
+  @ApiOkResponse({
+    description: '로그인 유저 이메일',
+    type: String,
+  })
+  testUser(@Req() req: Request & { user: { email: string } }) {
+    return `로그인 유저 이메일: ${req.user?.email}`;
   }
 }
